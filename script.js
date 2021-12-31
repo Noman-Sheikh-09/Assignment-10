01de44ab10568d3b7b7993bd7e6a2a6e1a53caac
noman <nomanshk310@gmail.com>
2022-01-01 00:33:55 +0500

    New

--- script.js
+++ script.js
@@ -416,6 +416,25 @@
 // where r is the radius and h is the height of the cylinder.
 
 
+// 7. Write a JavaScript program which returns a subset of a string.   
+// Sample Data: dog
+// Expected Output: ["d", "do", "dog", "o", "og", "g"]
+// var str  = function() 
+// {
+//   var subset = [];
+//   for (var m = 0; m < this.length; m++) 
+//   {
+//     for (var n = m+1; n<this.length+1; n++) 
+//     {
+//       subset.push(this.slice(m,n));
+//     }
+//   }
+//   return subset;
+// };
+
+// console.log("dog".sub_String());
+
+
 
 
 
